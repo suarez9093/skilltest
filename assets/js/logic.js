4d2f0bac7d0c84a1e9f223675eda7ffbab6bff1f
suarez9093 <suarez9093@hotmail.com>
2021-05-13 11:20:58 -0400

    added background color for body

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,10 +10,26 @@ async function getStories() {
       `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
     );
     const newStory = await data.json();
+    console.log('newStory ', newStory);
     const storiesContainer = document.getElementById('stories');
     const div = document.createElement('div');
-    div.innerHTML = `<p>${newStory.by}</p>`;
+    div.classList.add('stories__item');
+    div.innerHTML = `<p>${newStory.title}</p>
+    <p>${getPoints(newStory.score)} by ${newStory.by} ${
+      newStory.time
+    } minutes ago</p>`;
     storiesContainer.appendChild(div);
   });
 }
 getStories();
+
+function getPoints(num) {
+  switch (num) {
+    case num === 0:
+      return `${num} points`;
+    case num === 1:
+      return `${num} point`;
+    default:
+      return `${num} points`;
+  }
+}
